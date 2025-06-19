8b5baa18f577d699acdb29ba307b05ee6479dbb0
Rq4n <ryanlimabatista2@gmail.com>
2025-06-19 14:50:11 -0300

    Added setters and getters

---
+++ src/main/java/Account.java
@@ -0,0 +1,44 @@
+package main.java;
+
+public class Account {
+    //Usuarios, id, cpf, nome, senha, saldo
+    private int id;
+    private String cpf;
+    private String nome;
+    private String senha;
+    private double saldo;
+
+    public Account(String cpf, String nome, String senha){
+        this.cpf = cpf;
+        this.nome = nome;
+        this.senha = senha;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setSenha(){
+        this.senha = senha;
+    }
+    public String getSenha(){
+        return senha;
+    }
+    public double getSaldo(){
+        return saldo;
+    }
+    public String getCpf(){
+        return cpf;
+    }
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+    public int getId(){
+        return id;
+    }
+    @Override
+    public String toString(){
+        return "ID: "+id+" | Cpf: "+cpf+" | Nome: "+nome+" | Senha: "+senha+" | Saldo:"+saldo;
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+package main.java;
+
+public class Main {
+    public static void main(String[] args) {
+        Account conta = new Account("51694571890","Ryan","123123");
+        System.out.println(conta.toString());
+    }
+}
