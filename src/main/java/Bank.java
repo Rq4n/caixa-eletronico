4d68eadb3808a6adbd304bdace5b4310120fc6ca
Rq4n <ryanlimabatista2@gmail.com>
2025-06-23 19:59:45 -0300

    Removed transferirSaldo

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -4,18 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Bank {
-    //Gerenciador de contas
+
     private List<Account> contas = new ArrayList<>();
     private int contadorId = 1;
 
-    //Adicionar usuario
     public void criaUsuario(String nome, String cpf, String senha) {
-        Account novoUsuario = new Account(contadorId, nome, cpf, senha);
+        Account novoUsuario = new Account(contadorId, nome, cpf, senha, this);
         contas.add(novoUsuario);
         contadorId++;
     }
 
-    //Logar usuario
     public Account autenticar(String cpf, String senha) {
         for (Account contas : contas) {
             if (contas.getCpf().equals(cpf) && contas.getSenha().equals(senha)) {
@@ -26,18 +24,16 @@ public class Bank {
         return null;
     }
 
-    //Buscar por cpf
-    public void buscarPorCpf(String cpf) {
+    public Account buscarPorCpf(String cpf) {
         for (Account contas : contas) {
             if (cpf.equals(contas.getCpf())) {
-                System.out.println("CPF Existente");
-            } else {
-                System.out.println("CPF Inexistente");
+                return contas;
             }
         }
+        System.out.println("CPF Inexistente");
+        return null;
     }
 
-    //Remover usuario
     public void removerUsuario(int id) {
         boolean removida = contas.removeIf(contas -> contas.getId() == id);
         if (removida) {
@@ -52,35 +48,5 @@ public class Bank {
         for (Account contas : contas) {
             System.out.println(contas);
         }
-    }
-
-    public void transferirSaldo(Account contaOrigem, Account contaDestino, double valor) {
-        boolean encontrouOrigem = false;
-        for (Account conta : contas){
-            if (contaOrigem.getCpf().equals(contaOrigem)){
-                System.out.println("Conta origem valida");
-                encontrouOrigem = true;
-            }
-        }
-        if (!encontrouOrigem){
-            System.out.println("Conta origem invalida");
-        }
-        boolean encontrouDestino = false;
-        for (Account conta : contas){
-            if (conta.getCpf().equals(contaDestino)){
-                System.out.println("Conta destino valida");
-                encontrouDestino = true;
-                return;
-            }
-        }
-        if (!encontrouDestino){
-            System.out.println("Conta destino invalida");
-        }
-        if (contaOrigem.getSaldo() < valor){
-            System.out.println("Saldo insuficiente");
-            return;
-        }
-        contaOrigem.sacarSaldo(valor);
-        contaDestino.depositarSaldo(valor);
     }
 }
